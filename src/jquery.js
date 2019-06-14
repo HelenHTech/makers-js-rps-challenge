073dd555d616213a777c9af69f0bb130c79cb68e
Helen Hayward <helen.hayward@ft.com>
2019-06-14 16:49:44 +0100

    playerchoice method failing

--- src/jquery.js
+++ src/jquery.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
  
-  var rps = new RPS();
-  $('#newplayer1').text(rps._player1);
-  $('#newplayer2').text(rps._player2);
+  var rps = new Rps();
+
+    // $('#newplayer1').text(rps._player1);
+    // $('#newplayer2').text(rps._player2);
   
-  function updateResult() {
-    rps.gamerules();
-    $('#result').test(rps._result)
-  }
+    $('.player1_choice').on('click', function() {
+      console.log('test')
+    rps.game('rock', 'paper');
+    $('#result').text(rps._result)
+  })
 
 });
--- src/rps.js
+++ src/rps.js
@@ -1,21 +1,22 @@
-function RPS() {
+function Rps() {
   this._player1 = document.getElementById('newplayer1').innerHTML;
   this._player2 = document.getElementById('newplayer2').innerHTML;
   this._result = "";
+  this._rules = { 'rock':'scissors', 'paper':'rock', 'scissors':'paper' };
 }
 
 function playerChoice(player, choice) {
   document.getElementById(player).value = choice;
 }
 
-RPS.prototype.gamerules = function() {
-  this._rules = { 'Rock':'Scissors', 'Paper':'Rock', 'Scissors':'Paper'};
-  if (player1.choice == player2.choice) {
-    result = "It's a draw this time";
-  } else if (this._rules[player1.choice] == player2.choice) {
-      result = "Congratulations" + player1 + ", you won";
+Rps.prototype.game = function(player1choice, player2choice) {
+  player1choice = (document.getElementById("player1").value)
+  player2choice = (document.getElementById("player2").value)
+  if (player1choice === player2choice) {
+    this._result.textContent = "It's a draw this time";
+  } else if (this._rules[player1choice] === player2choice) {
+    this._result.textContent = "Congratulations ${player1}, you won";
   } else {
-    result = "Congratulations" + player2 + ", you won";
+    this._result.textContent = "Congratulations ${player2}, you won";
   }
-  
-};
+}
